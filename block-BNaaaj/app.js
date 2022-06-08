7b15edb8cb75ddb7bc3dce6a7945e2b5efe489bf
vikas2489 <vk55002678@gmail.com>
2022-06-08 19:30:20 +0530

    completed the assignment

---
+++ block-BNaaaj/app.js
@@ -0,0 +1,8 @@
+// In app.js, require functions from area.js and execute 
+// the areas of given figures and console.log the results.
+
+let allAreas = require("./area");
+
+console.log(allAreas.areaOfCircle(2));
+console.log(allAreas.areaOfSquare(2));
+console.log(allAreas.areaOfRectangle(2, 4));
