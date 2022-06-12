ee9575484044adf22d33a2ac931e69a9e721d454
vikas2489 <vk55002678@gmail.com>
2022-06-12 23:53:48 +0530

    completed finally

--- block-BNaaav/index.js
+++ block-BNaaav/index.js
@@ -5,15 +5,20 @@ let server = http.createServer(handleRequest);
 
 function handleRequest(req, res) {
     if (req.url === "/" && req.method === "GET") {
-        // fs.readFile(`./Vikas-Singh-Portfolio/index.html`, (err, content) => {
-        //         res.setHeader("content-type", "text/html");
-        //         console.log("vikas");
-        //         console.log(err);
-        //         res.end(content);
-        //     })
         res.setHeader("content-type", "text/html");
         fs.createReadStream(`./Vikas-Singh-Portfolio/index.html`).pipe(res);
-        fs.createReadStream(`./Vikas-Singh-Portfolio/Assets`).pipe(res);
+    } else if (req.url.split('.').pop() === 'css') {
+        res.setHeader('Content-Type', 'text/css');
+        fs.readFile('./Vikas-Singh-Portfolio/assets/Stylesheet/' + req.url, (err, content) => {
+            if (err) return console.log(err);
+            res.end(content);
+        });
+    } else if (req.url === "/projects" && req.method === "GET") {
+        res.setHeader("content-type", "text/html");
+        fs.createReadStream(`./Vikas-Singh-Portfolio/projects.html`).pipe(res);
+    } else if (req.url === "/my-pic" && req.method === "GET") {
+        res.setHeader("content-type", "image/jpeg");
+        fs.createReadStream(`./Vikas-Singh-Portfolio/Assets/images/my-pic.jpeg`).pipe(res);
     }
 }
 
